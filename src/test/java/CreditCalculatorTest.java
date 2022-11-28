6ff42e579bde694d931e645db3101164839de903
vlifanova <vlifanova2@alfabank.ru>
2022-11-28 09:43:28 +0300

    Credit Calculator

--- src/test/java/CreditCalculatorTest.java
+++ src/test/java/CreditCalculatorTest.java
@@ -12,14 +12,13 @@ public class CreditCalculatorTest {
         Random random = new Random();
         int generalAmount = random.nextInt(500_000) + 1_000; // общая сумма кредита
         int termCredit = random.nextInt(10) + 1; // срок кредита
-        int procent = random.nextInt(10) + 5; // процент кредита
+        int percent = random.nextInt(10) + 5; // процент кредита
 
+        int expectedMonthlyPayment = (generalAmount + (generalAmount * percent * termCredit / 100)) / (termCredit * 12);
 
-        int expectedMonathlyPayment = (generalAmount + (generalAmount * procent * termCredit / 100)) / (termCredit * 12);
+        int monthlyPayment = creditCalculator.calculateMonthlyPayment(generalAmount, termCredit, percent);
 
-        int monthlyPayment = creditCalculator.calculateMonthlyPayment(generalAmount, termCredit, procent);
-
-        Assertions.assertEquals(expectedMonathlyPayment, monthlyPayment);
+        Assertions.assertEquals(expectedMonthlyPayment, monthlyPayment);
     }
 
     @Test
@@ -29,11 +28,11 @@ public class CreditCalculatorTest {
         Random random = new Random();
         int generalAmount = random.nextInt(500_000) + 1_000;
         int termCredit = random.nextInt(10) + 1;
-        int procent = random.nextInt(10) + 5;
+        int percent = random.nextInt(10) + 5;
 
-        int expectedGeneralSum = generalAmount + (generalAmount * procent * termCredit / 100);
+        int expectedGeneralSum = generalAmount + (generalAmount * percent * termCredit / 100);
 
-        int generalSum = creditCalculator.calculateGeneralSum(generalAmount, termCredit, procent);
+        int generalSum = creditCalculator.calculateGeneralSum(generalAmount, termCredit, percent);
 
         Assertions.assertEquals(expectedGeneralSum, generalSum);
     }
@@ -45,11 +44,11 @@ public class CreditCalculatorTest {
         Random random = new Random();
         int generalAmount = random.nextInt(500_000) + 1_000;
         int termCredit = random.nextInt(10) + 1;
-        int procent = random.nextInt(10) + 5;
+        int percent = random.nextInt(10) + 5;
 
-        int expectedOverPayment = (generalAmount * procent * termCredit / 100);
+        int expectedOverPayment = (generalAmount * percent * termCredit / 100);
 
-        int overPayment = creditCalculator.calculateOverPayment(generalAmount, termCredit, procent);
+        int overPayment = creditCalculator.calculateOverPayment(generalAmount, termCredit, percent);
 
         Assertions.assertEquals(expectedOverPayment, overPayment);
     }
